a4ebe88dabfaf59afda75be454e4f9d4e6f1823a
sela <sela@jenkins-1-4f6d.c.devops-course-architecture.internal>
2025-06-08 10:53:59 +0000

    upgrade app version

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+  const ip = req.socket.remoteAddress;
+  console.log(`Request received from IP: ${ip}`);
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World 4!\n');
+  res.end('Hello, World! this is the new version. it logs connections as well\n');
 });
 
 server.listen(port, hostname, () => {
